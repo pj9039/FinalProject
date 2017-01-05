d30ed8aeaaec82c537c0438909cf4ced44fde982
pj9039 <kms_14@naver.com>
2017-01-05 18:36:56 +0900

    포관손익계산서 부분데이터 추출완료

--- parser(jaemujepyo).py
+++ parser(jaemujepyo).py
@@ -1,11 +1,12 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import DataDeclare
 code =['005930']
 for c in code:
     url="http://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A"+c+"&cID=&MenuYn=Y&ReportGB=&NewMenuID=103&stkGb=701"
-
     f = urllib.request.urlopen(url).read()
     soup=BeautifulSoup(f, 'html.parser')
+    '''
     print('----------------------------------------------')
     cells = soup.find('tr').find_all('th')
     print(cells)
@@ -13,8 +14,30 @@ for c in code:
         print(cell.string)
     print('----------------------------------------------')
     print('EPS')
-    cells = soup.find('tr', {'id': "p_grid1_6"}).find_all('td')
+    cells = soup.find('tr', {'id': "p_grid1_4"}).find_all('td')
     print(cells)
     for cell in cells:
         print (cell.string)
+    '''
+    print('----------------------------------------------')
+    cells = soup.find('div', {'id': "divSonikY"}).find_all('td')
+    #print(cells)
+    a = []
+    for cell in cells:
+        #print (cell.string)
+        a.append(cell.string)
+    print(a)
+    print(len(a))
+    matrix = DataDeclare.setmatrix()
+    k=0
+    for i in range(0,74):
+        for j in range(1,7):
+            matrix[i][j] = a[(j-1)+k]
+        if k!=422:
+            k=k+6
+    for row in matrix:
+        print(row)
+
+
+
 
