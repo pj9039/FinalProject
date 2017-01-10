52946e3e4504b9b4b8d0a3fdab2054366e6873ee
pj9039 <kms_14@naver.com>
2017-01-10 18:48:33 +0900

    모든종목 EPS DB에 넣기 완료

--- parser(tujajipyo).py
+++ parser(tujajipyo).py
@@ -1,90 +1,42 @@
 import urllib.request
 from bs4 import BeautifulSoup
-code =['005930']
+import mysqlinfo
+import pymysql
+import Allcode
+myhost,myport, myuser, mypassword, mydb, mycharset = mysqlinfo.getmysqlinfo()
+conn = pymysql.connect(host=myhost,port=myport, user=myuser, password=mypassword, db=mydb, charset=mycharset)
+
+
+code = Allcode.getcode()
 for c in code:
-    url="http://comp.fnguide.com/SVO2/ASP/SVD_Invest.asp?pGB=1&gicode=A"+c+"&cID=&MenuYn=Y&ReportGB=&NewMenuID=105&stkGb=701%22"
+    try:
+        url="http://comp.fnguide.com/SVO2/ASP/SVD_Invest.asp?pGB=1&gicode=A"+c+"&cID=&MenuYn=Y&ReportGB=&NewMenuID=105&stkGb=701%22"
+        print(c)
+        f = urllib.request.urlopen(url).read()
+        soup=BeautifulSoup(f, 'html.parser')
+        print('----------------------------------------------')
 
-    f = urllib.request.urlopen(url).read()
-    soup=BeautifulSoup(f, 'html.parser')
-    print('----------------------------------------------')
-    cells = soup.find('tr').find_all('th')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('EPS')
-    cells = soup.find('tr', {'id': "p_grid1_1"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print (cell.string)
-    print('----------------------------------------------')
-    print('EBITDAPS')
-    cells = soup.find('tr', {'id': "p_grid1_2"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('CFPS')
-    cells = soup.find('tr', {'id': "p_grid1_3"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('SPS')
-    cells = soup.find('tr', {'id': "p_grid1_4"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('BPS')
-    cells = soup.find('tr', {'id': "p_grid1_5"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
+        cells = soup.find('tr').find_all('th')
 
-    print('----------------------------------------------')
-    print('DPS')
-    cells = soup.find('tr', {'id': "p_grid1_6"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
+        date = []
+        for cell in cells:
+            date.append(cell.string)
+        print('----------------------------------------------')
+        print('EPS')
+        cells = soup.find('tr', {'id': "p_grid1_1"}).find_all('td')
+        eps = []
+        for cell in cells:
+            eps.append(cell.string)
 
-    print('----------------------------------------------')
-    print('배당성향')
-    cells = soup.find('tr', {'id': "p_grid1_8"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('PER')
-    cells = soup.find('tr', {'id': "p_grid1_9"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('PCR')
-    cells = soup.find('tr', {'id': "p_grid1_10"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('PSR')
-    cells = soup.find('tr', {'id': "p_grid1_11"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
+        for i in range(1,6):
+            with conn.cursor() as curs:
+                sql = "INSERT INTO eps(shcode, gsym,epsvalue) VALUES (%s, %s,%s)"
+                curs.execute(sql, (c, date[i] + '/01', eps[i-1]))
 
-    print('----------------------------------------------')
-    print('PBR')
-    cells = soup.find('tr', {'id': "p_grid1_12"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
-    print('----------------------------------------------')
-    print('EV/EBITDA')
-    cells = soup.find('tr', {'id': "p_grid1_14"}).find_all('td')
-    print(cells)
-    for cell in cells:
-        print(cell.string)
+            conn.commit()
+    except:
+        pass
+
+conn.close()
 
 
