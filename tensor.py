2eac953d3f6c821d00cfe2f874332dca01c9e878
pj9039 <kms_14@naver.com>
2017-03-15 11:01:46 +0900

    변수들을 2개씩조합해서 전부 비교해봄 출력은 테스트셋중 몇개 맞았는지 출력

--- tensor.py
+++ tensor.py
@@ -14,71 +14,93 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
+def MakeVar(data):
+    Var = []
+    FinalData = []
+    x_data = data[3:-1].astype(np.float32)  # 3행 6열
+    basis = x_data[0]
+    #y_data = data[-1].astype(np.float32)  # 1행 6열
+    x_data = preprocessing.normalize(x_data[1:], norm='l2')
+    #x_data = np.insert(x_data, 0, basis, 0)
+    for i in range(0, len(x_data)):
+        for j in range(0, len(x_data)):
+            if (i >= j):
+                pass
+            else:
+                Var.append(basis)
+                Var.append(x_data[i])
+                Var.append(x_data[j])
+                #Var.append(y_data)
+                FinalData.append((Var))
+                Var = []
+    return FinalData
+
+
+
+
 df = pd.read_excel('BankruptcyStock.xls')
 xy = df.as_matrix().transpose()
 
 # print(xy[0], xy[-1])        # [ 1.  1.  1.  1.  1.  1.] [ 0.  0.  0.  1.  1.  1.]
 
-x_data = xy[3:-1].astype(np.float32)            # 3행 6열
+
+
+#x_data = xy[3:-1].astype(np.float32)            # 3행 6열
 y_data = xy[-1].astype(np.float32)             # 1행 6열
-basis = x_data[0]
-x_data = preprocessing.normalize(x_data[5:], norm='l2')
-x_data = np.insert(x_data, 0, basis, 0)
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
-print(x_data)
+#basis = x_data[0]
+#x_data = preprocessing.normalize(x_data[5:], norm='l2')
+#x_data = np.insert(x_data, 0, basis, 0)
 
-# feature별 가중치를 난수로 초기화. feature는 bias 포함해서 3개. 1행 3열.
-W = tf.Variable(tf.random_uniform([1, len(x_data)], -1.0, 1.0))
+x_data = MakeVar(xy)
 
-# 행렬 곱셈. (1x3) * (3x6)
-h = tf.matmul(W, X)
-hypothesis = tf.div(1., 1. + tf.exp(-h))    # exp(-h) = e ** -h. e는 자연상수
+for i in range(0,len(x_data)):
+    X = tf.placeholder(tf.float32)
+    Y = tf.placeholder(tf.float32)
 
-# exp()에는 실수만 전달
-# print(tf.exp([1., 2., 3.]).eval())      # [2.71828175 7.38905621 20.08553696]
-# print(tf.exp([-1., -2., -3.]).eval())   # [0.36787945 0.13533528 0.04978707]
+    W = tf.Variable(tf.random_uniform([1, len(x_data[0])], -1.0, 1.0))
 
-cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+    h = tf.matmul(W, X)
+    hypothesis = tf.div(1., 1. + tf.exp(-h))    # exp(-h) = e ** -h. e는 자연상수
 
-rate = tf.Variable(0.2)
-optimizer = tf.train.GradientDescentOptimizer(rate)
-train = optimizer.minimize(cost)
+    cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
 
-init = tf.global_variables_initializer()
+    rate = tf.Variable(0.2)
+    optimizer = tf.train.GradientDescentOptimizer(rate)
+    train = optimizer.minimize(cost)
 
-sess = tf.Session()
-sess.run(init)
+    init = tf.global_variables_initializer()
 
-for step in range(2001):
-    sess.run(train, feed_dict={X: x_data, Y: y_data})
-    if step % 20 == 0:
-        print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W))
+    sess = tf.Session()
+    sess.run(init)
 
-print('-----------------------------------------')
+    for step in range(2001):
+        sess.run(train, feed_dict={X: x_data[i], Y: y_data})
+        if step % 20 == 0:
+            sess.run(cost, feed_dict={X: x_data[i], Y: y_data}), sess.run(W)
+            #print(step, sess.run(cost, feed_dict={X: x_data[i], Y: y_data}), sess.run(W))
 
-# 결과가 0 또는 1로 계산되는 것이 아니라 0과 1 사이의 값으로 나오기 때문에 True/False는 직접 판단
+    print('-----------------------------------------')
 
-df = pd.read_excel('BankruptcyStock(test).xls')
-xy = df.as_matrix().transpose()
+    # 결과가 0 또는 1로 계산되는 것이 아니라 0과 1 사이의 값으로 나오기 때문에 True/False는 직접 판단
 
-# print(xy[0], xy[-1])        # [ 1.  1.  1.  1.  1.  1.] [ 0.  0.  0.  1.  1.  1.]
-
-x_data = xy[3:-1].astype(np.float32)            # 3행 6열
-basis = x_data[0]
-x_data = preprocessing.normalize(x_data[5:], norm='l2')
-x_data = np.insert(x_data, 0, basis, 0)
-
-y_data = xy[-1].astype(np.float32)             # 1행 6열
-
-for i in range(0, len(x_data[0])):
-    #y_pred = sess.run(hypothesis , feed_dict={X: x_data})
-    #correct_prediction = tf.equal(tf.cast(tf.argmax(hypothesis,1), tf.float32), Y)
-    #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    #print ("Accuracy: ",(sess.run(accuracy, feed_dict={X: x_data, Y: y_data})))
-    print('[', x_data[0][i], x_data[1][i], x_data[2][i], x_data[3][i], y_data[i],']: ',sess.run(hypothesis, feed_dict={X: [[x_data[0][i]], [x_data[1][i]], [x_data[2][i]], [x_data[3][i]] ]}) > 0.8)
-    #print('[', x_data[0][i], x_data[1][i], x_data[2][i], y_data[i],']: ',sess.run(hypothesis, feed_dict={X: [[x_data[0][i]], [x_data[1][i]], [x_data[2][i]] ]}) > 0.8 )
+    df = pd.read_excel('BankruptcyStock(test).xls')
+    xy = df.as_matrix().transpose()
 
 
-sess.close()
+
+    x_data = MakeVar(xy)
+    y_data = xy[-1].astype(np.float32)             # 1행 6열
+
+    score = 0
+    for j in range(0,len(x_data[0][0])):
+
+
+        #print('[', x_data[i][0][j], x_data[i][1][j], x_data[i][2][j], y_data[j],']: ',sess.run(hypothesis, feed_dict={X: [[x_data[i][0][j]], [x_data[i][1][j]], [x_data[i][2][j]] ]}) > 0.7)
+        if((sess.run(hypothesis, feed_dict={X: [[x_data[i][0][j]], [x_data[i][1][j]], [x_data[i][2][j]] ]}) > 0.5)[0][0] == y_data[j]):
+            score = score + 1
+
+    print(len(x_data[0][0]), '중', score,'점')
+
+
+    sess.close()
 
