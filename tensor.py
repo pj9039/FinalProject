5eb5c6554b84c9a2a66f3e3a2e5302c6fdc40c74
pj9039 <kms_14@naver.com>
2017-03-15 17:19:45 +0900

    변수가2개일경우 각 변수별 스코어확인가능

--- tensor.py
+++ tensor.py
@@ -19,9 +19,7 @@ def MakeVar(data):
     FinalData = []
     x_data = data[3:-1].astype(np.float32)  # 3행 6열
     basis = x_data[0]
-    #y_data = data[-1].astype(np.float32)  # 1행 6열
     x_data = preprocessing.normalize(x_data[1:], norm='l2')
-    #x_data = np.insert(x_data, 0, basis, 0)
     for i in range(0, len(x_data)):
         for j in range(0, len(x_data)):
             if (i >= j):
@@ -30,7 +28,8 @@ def MakeVar(data):
                 Var.append(basis)
                 Var.append(x_data[i])
                 Var.append(x_data[j])
-                #Var.append(y_data)
+                Var.append(i)
+                Var.append(j)
                 FinalData.append((Var))
                 Var = []
     return FinalData
@@ -39,32 +38,33 @@ def MakeVar(data):
 
 
 df = pd.read_excel('BankruptcyStock.xls')
+df1 = pd.read_excel('BankruptcyStock.xls', header=None)
 xy = df.as_matrix().transpose()
+xy1 = df1.as_matrix().transpose()
 
-# print(xy[0], xy[-1])        # [ 1.  1.  1.  1.  1.  1.] [ 0.  0.  0.  1.  1.  1.]
+x_data1 = xy1[1:][3:-1] # 3행 6열
 
 
 
 #x_data = xy[3:-1].astype(np.float32)            # 3행 6열
 y_data = xy[-1].astype(np.float32)             # 1행 6열
-#basis = x_data[0]
-#x_data = preprocessing.normalize(x_data[5:], norm='l2')
-#x_data = np.insert(x_data, 0, basis, 0)
-
 x_data = MakeVar(xy)
+x_dataTitle = []
+for i in range(0,len(x_data1)):
+    x_dataTitle.append(x_data1[i][:1])
 
 for i in range(0,len(x_data)):
     X = tf.placeholder(tf.float32)
     Y = tf.placeholder(tf.float32)
 
-    W = tf.Variable(tf.random_uniform([1, len(x_data[0])], -1.0, 1.0))
+    W = tf.Variable(tf.random_uniform([1, len(x_data[0][:-2])], -1.0, 1.0))
 
     h = tf.matmul(W, X)
     hypothesis = tf.div(1., 1. + tf.exp(-h))    # exp(-h) = e ** -h. e는 자연상수
 
     cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
 
-    rate = tf.Variable(0.2)
+    rate = tf.Variable(0.1)
     optimizer = tf.train.GradientDescentOptimizer(rate)
     train = optimizer.minimize(cost)
 
@@ -73,11 +73,11 @@ for i in range(0,len(x_data)):
     sess = tf.Session()
     sess.run(init)
 
-    for step in range(2001):
-        sess.run(train, feed_dict={X: x_data[i], Y: y_data})
+    for step in range(3001):
+        sess.run(train, feed_dict={X: x_data[i][:-2], Y: y_data})
         if step % 20 == 0:
-            sess.run(cost, feed_dict={X: x_data[i], Y: y_data}), sess.run(W)
-            #print(step, sess.run(cost, feed_dict={X: x_data[i], Y: y_data}), sess.run(W))
+            sess.run(cost, feed_dict={X: x_data[i][:-2], Y: y_data}), sess.run(W)
+            #print(step, sess.run(cost, feed_dict={X: x_data[i][:-2], Y: y_data}), sess.run(W))
 
     print('-----------------------------------------')
 
@@ -88,18 +88,17 @@ for i in range(0,len(x_data)):
 
 
 
-    x_data = MakeVar(xy)
-    y_data = xy[-1].astype(np.float32)             # 1행 6열
+    x_dataTest = MakeVar(xy)
+    y_dataTest = xy[-1].astype(np.float32)             # 1행 6열
 
     score = 0
-    for j in range(0,len(x_data[0][0])):
+    for j in range(0,len(x_dataTest[0][0])):
 
+        #print('[', x_dataTest[i][0][j], x_dataTest[i][1][j], x_dataTest[i][2][j], y_dataTest[j],']: ',sess.run(hypothesis, feed_dict={X: [[x_dataTest[i][0][j]], [x_dataTest[i][1][j]], [x_dataTest[i][2][j]] ]}) > 0.7)
 
-        #print('[', x_data[i][0][j], x_data[i][1][j], x_data[i][2][j], y_data[j],']: ',sess.run(hypothesis, feed_dict={X: [[x_data[i][0][j]], [x_data[i][1][j]], [x_data[i][2][j]] ]}) > 0.7)
-        if((sess.run(hypothesis, feed_dict={X: [[x_data[i][0][j]], [x_data[i][1][j]], [x_data[i][2][j]] ]}) > 0.5)[0][0] == y_data[j]):
+        if((sess.run(hypothesis, feed_dict={X: [[x_dataTest[i][0][j]], [x_dataTest[i][1][j]], [x_dataTest[i][2][j]] ]}) > 0.6)[0][0] == y_dataTest[j]):
             score = score + 1
-
-    print(len(x_data[0][0]), '중', score,'점')
+    print('변수1: ',x_dataTitle[x_data[i][-2]],'변수2: ',x_dataTitle[x_data[i][-1]], '점수: ', len(x_dataTest[0][0]), '중', score,'점')
 
 
     sess.close()
