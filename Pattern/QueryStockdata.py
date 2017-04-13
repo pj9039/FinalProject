60a47c18500029b658cf9841900d2e5e1a53acb4
pj9039 <kms_14@naver.com>
2017-04-13 19:56:57 +0900

    QueryStockdata.py 수정 함수 2개(makequerylist, getlastestdate) 추가 makequerylist : 웹에서 넘긴 가격을 비교하여 종목을 추려내는 함수 getlastestdate : DB에 들어간 최신날짜를 반환하는 함수

--- Pattern/QueryStockdata.py
+++ Pattern/QueryStockdata.py
@@ -2,7 +2,7 @@ import pymysql
 import mysqlinfo
 
 
-# Get stockprice from Database
+# Get stockprice from Database by stock code
 def getstockprice(shcode):
     # Open database connection
     myhost, myport, myuser, mypassword, mydb, mycharset = mysqlinfo.getmysqlinfo()
@@ -23,3 +23,52 @@ def getstockprice(shcode):
 
     # End of function
     return rows
+
+
+# Get stockprice from Database by prices
+def makequerylist(p_low, p_high):
+    # Open database connection
+    myhost, myport, myuser, mypassword, mydb, mycharset = mysqlinfo.getmysqlinfo()
+    conn = pymysql.connect(host=myhost, port=myport, user=myuser, password=mypassword, db=mydb, charset=mycharset)
+
+    # create Dictionary Cursor from connection
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # Execute SQL
+    sql = "SELECT DISTINCT(shcode) FROM stockprice WHERE  %s < closeprice AND closeprice < %s AND marketdate=%s"
+    cursor.execute(sql, p_low, p_high, getlastestdate())
+
+    # disconnect
+    conn.close()
+
+    # Fetch & cleansing
+    rows = cursor.fetchall()
+    array = []
+    for row in rows:
+      array.append(row['shcode'])
+
+    # End of function
+    return array
+
+
+# Get lastest date from Database
+def getlastestdate():
+    # Open database connection
+    myhost, myport, myuser, mypassword, mydb, mycharset = mysqlinfo.getmysqlinfo()
+    conn = pymysql.connect(host=myhost, port=myport, user=myuser, password=mypassword, db=mydb, charset=mycharset)
+
+    # create Dictionary Cursor from connection
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # Execute SQL
+    sql = "SELECT marketdate from stockprice order by marketdate desc limit 1"
+    cursor.execute(sql)
+
+    # disconnect
+    conn.close()
+
+    # Fetch & cleansing
+    row = cursor.fetchall()
+
+    # End of function
+    return row
