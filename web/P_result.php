2c94f6da3cabae743e71333d673ba614e987bf03
dbqhd345 <dbqhd345@naver.com>
2017-04-15 18:29:15 +0900

    디자인수정
    디자인수정 & 샘플그림 추가, 클릭이벤트 추가

--- web/P_result.php
+++ web/P_result.php
@@ -10,22 +10,42 @@
 
 <!--페이징 변수 정의-->
 <?
+
+	//select marketdate from stockprice order by marketdate desc limit 1;	//가장 최근 날짜 조회
+	//select * from stockprice where marketdate='변수(2017-01-10)'
+	//$shcode = $_POST['shcode'];
+	
+	// 검색 날짜 조건 설정
+	$now_date = "SELECT marketdate from stockprice order by marketdate desc limit 1";
+	// 가장 최근 날짜
+	$date = mysqli_query($db,"SELECT marketdate from stockprice order by marketdate desc limit 1");
+	$date_data = mysqli_fetch_row($date);
+	$date_result = $date_data[0];
+
+
 	$pageNum = ($_GET['page']) ? $_GET['page'] : 1;	//초기값 1로 지정
 	$list = ($_GET['list']) ? $_GET['list'] : 5;	//한 페이지에 5개씩 보여주기
 	$b_pageNum_list = 10;	//블록에 나타낼 페이지 번호 갯수
 	$block = ceil($pageNum/$b_pageNum_list);	//현재 리스트의 블록을 구하는 식
 	$b_start_page = (($block-1) * $b_pageNum_list) + 1;	//현재 블럭에서 시작페이지 번호
 	$b_end_page = $b_start_page + $b_pageNum_list - 1;	//현재 블럭에서 마지막 페이지 번호
-	$tot_count_query = mysqli_query($db,"SELECT count(*) FROM stockprice");	//행의 개수
+	$tot_count_query = mysqli_query($db,"SELECT count(*) FROM stockprice where marketdate='".$date_result."'");	//행의 개수
 	$rows = mysqli_fetch_array($tot_count_query);
 	$total_count = $rows[0];
 	$total_page = ceil($total_count / $list);	//총 행의 페이지 수
 	$total_block = ceil($total_page / $b_pageNum_list);	//block의 총 갯수
 	$start_list = $list*($pageNum-1);	// 현재화면에서 시작 항목 번호
 
+
 if ($b_end_page > $total_page){	//마지막 페이지가 총 페이지보다 클 때 같게 해주는 조건
 		$b_end_page = $total_page;
 	}
+
+// 리스트 출력을 위한 쿼리
+		$date_query = "SELECT * FROM stockprice where marketdate='".$date_result."' limit ".$start_list.", ".$list;
+		
+		// 가장 최근날짜 검색 결과
+		$result = mysqli_query($db,$date_query);
 		
 ?>
 <HTML>
@@ -33,8 +53,13 @@ if ($b_end_page > $total_page){	//마지막 페이지가 총 페이지보다 클
 <HEAD>
 	<title>개미추적자</title>
 
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="./js/bootstrap.min.js"></script>
+
 	<style type="text/css">
-		table, td{
+/*		table, td{
 			border:1px;
 		}
 		table{
@@ -42,93 +67,170 @@ if ($b_end_page > $total_page){	//마지막 페이지가 총 페이지보다 클
 			height: 100px;
 			margin: auto;
 			text-align: center;
-		}
+		}*/
 	a:link { color: red; text-decoration: none;}
  	a:visited { color: black; text-decoration: none;}
  	a:hover { color: blue; text-decoration: underline;}
 	</style>
-	
+<style type="text/css">
+	th,td{padding: 15px;}
+	table{border-spacing: 5px;}
+	img:hover {
+   padding:1px;
+   border:1px solid #021a40;
+   background-color:#ff0;
+	}
+	.activeImg{
+		padding:1px;
+   border:1px solid #021a40;
+   background-color:#ff0;
+	}
+</style>
+<script>
+	function borderClick(bId){
+			var img = document.getElementById(bId);
+
+			for(var i = 1; i < 10; i++){
+				var imgTmp = document.getElementById("img"+i);
+				imgTmp.classList.remove("activeImg");
+			}
+
+			img.classList.add("activeImg");
+	}
+</script>
 </HEAD>
 <body>
 <center>
 	<BR>
 	<H1>그래프검색</H1>
 	<BR>
-	<!--그래프검색-->
-	<input type=button value=메인화면 onclick="location.href='main.php';"/><BR><BR>
 	<HR>
-	<form action="graph.php" method="post">
-	<!--tr:한줄 th:글씨 진하게 td:글씨보통 colspan:가로줄을 합치는 개수 rowspan:세로줄을 합치는 개수-->
-	<table border=0 cellpadding=5 cellspacing=1>
+	<form action="P_result.php" method="post">
+	<table style="width:1200">
 		<tr>
-			<th colspan="3" align="left"><font size="5"/>그래프</th>
-			<th colspan="3" align="left"><font size="5"/>추가조건</th>
+			<th align="center"><font size="5"/><span class="glyphicon glyphicon-check"></span> 그래프</th>
+			<th align="center"><font size="5"/><span class="glyphicon glyphicon-search"></span> 추가조건</th>
 		</tr>
-
-		<tr>
-			<td>그래프1</td>
-			<td>그래프2</td>
-			<td>그래프3</td>
-			<td> <input type=text name=price1/> </td>
-			<td> < 가격 < </td>
-			<td> <input type=text name=price2/> </td>
-		</tr>
-
-		<tr>
-			<td>그래프4</td>
-			<td>그래프5</td>
-			<td>그래프6</td>
-			<td> <input type=text name=volume1/> </td>
-			<td> < ROE < </td>
-			<td> <input type=text name=volume2/> </td>
-		</tr>
-
-		<tr>
-			<td>그래프7</td>
-			<td>그래프8</td>
-			<td>그래프9</td>
-			<td colspan="3" style="word-spacing: 15px"> <input type="submit" value="검색"> <input type="reset" value="리셋"> </td>
-		</tr>
-
 	</table>
+	<!--tr:한줄 th:글씨 진하게 td:글씨보통 colspan:가로줄을 합치는 개수 rowspan:세로줄을 합치는 개수-->
+	<div style="display:table">
+	<div style="display:table-cell; text-align:center">
+	<div style="width: 600px; height: 500px; border: 1px; float: left;">
+	<table width="600" height="500" border="0" style="text-align: center;">
+	<tr>
+		<td>
+			<a href="javascript:borderClick('img1')"><img id="img1" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image1"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img2')"><img id="img2" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image2"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img3')"><img id="img3" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image3"/> -->
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<a href="javascript:borderClick('img4')"><img id="img4" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image4"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img5')"><img id="img5" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image5"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img6')"><img id="img6" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image6"/> -->
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<a href="javascript:borderClick('img7')"><img id="img7" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image7"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img8')"><img id="img8" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image8"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img9')"><img id="img9" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image9"/> -->
+		</td>
+	</tr>
+</table>
+	</div>
+	<div style="position: relative; width: 600px; height: 500px; border: 1px;  float: left; display:table;">
+	<div style="display:table-cell;vertical-align:middle;">
+	<table width="600" height="500" border="0" text-align: center;">
+	<tr style="vertical-align:bottom;">
+		<td colspan="3" style="text-align:left;"><font size="5"><span class="glyphicon glyphicon-expand"></span> 가격</font></td>
+	</tr>
+	<tr style="vertical-align:top;">
+		<td><input class="form-control" type=text name="price1" size="20" /></td>
+		<td><font size="5"/> ~ </td>
+		<td><input class="form-control" type=text name="price2" size="20"/></td>
+	</tr>
+	<tr style="vertical-align:bottom;">
+		<td colspan="3" style="text-align:left;"><font size="5"><span class="glyphicon glyphicon-expand"></span> ROE</font></td>
+	</tr>
+	<tr style="vertical-align:top;">
+			<td> <input class="form-control" type=text name="roe1" size="20"/> </td>
+			<td><font size="5"/> ~ </td>
+			<td> <input class="form-control" type=text name="roe2" size="20" /> </td>
+	</tr>
+</table>
+</div>
+</div>
+</div>
+</div>
+<table style="width:1200">
+	<tr>
+		<td  style="word-spacing: 15px" align="center"><input type="submit" class="btn btn-default" style="width: 300px; height:50px" value=검색> </td>	
+		<td  style="word-spacing: 15px" align="center"><input type="reset" class="btn btn-default" style="width: 300px; height: 50px" value=리셋> </td>
+	</tr>
+</table>
+	</form>
 
 	<HR>
 	<H2>검색결과</H2>
 	<a href="graph.php">되돌아가기</a>
 	<br><br><br>
-		<table style="border:1px solid #bcbcbc;">
-		<tr>
-			<th><font size="5"/>번호</th>
-			<th><font size="5"/>종목코드</th>
-			<th><font size="5"/>날짜</th>
-			<th><font size="5"/>종가</th>
-		</tr>
-
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
+		<!-- <table style="border:1px solid #bcbcbc;"> -->
+		<table class="table" style="width:80%;">
+		<tr style="background-color:#EAEAEA;border-bottom:2px solid black;border-top:2px solid black;">
+			<th style="text-align:center;"><font size="5"/>번호</th>
+			<th style="text-align:center;"><font size="5"/>종목코드</th>
+			<th style="text-align:center;"><font size="5"/>날짜</th>
+			<th style="text-align:center;"><font size="5"/>종가</th>
 		</tr>
 
 	<?
-		//select marketdate from stockprice order by marketdate desc limit 1;	//가장 최근 날짜 조회
-		//select * from stockprice where marketdate='변수(2017-01-10)'
-		#$shcode = $_POST['shcode'];
-		$result = mysqli_query($db,"SELECT * FROM stockprice limit 1".$start_list.", ".$list);
 		if (mysqli_num_rows($result) > 0) {
     	// output data of each row
+    	$i = 1;
 			while($row = mysqli_fetch_assoc($result)) {
-				?>
-				<tr>
+				if($i%2 == 0){
+	?>
+				<tr style="background-color:white;">
+	<?
+				} else {
+	?>
+					<tr>
+	<?
+				}
+	?>
+				
 					<td style="border:1px solid #bcbcbc;"><a href="result.php"/><? echo $row["idx"] ?></a></td>
 					<td style="border:1px solid #bcbcbc;"><? echo $row["shcode"] ?></td>
 					<td style="border:1px solid #bcbcbc;"><? echo $row["marketdate"] ?></td>
 					<td style="border:1px solid #bcbcbc;"><? echo $row["closeprice"] ?></td>
 				</tr>
 				<?
+				$i++;
 			}
-		  }
+		}
 		?>
 	</table>
 	<table>
@@ -173,14 +275,12 @@ if ($b_end_page > $total_page){	//마지막 페이지가 총 페이지보다 클
 			</td>
 		</tr>
 	</table>
-
+	<?
+		 $price1 = $_POST['price1'];
+		 $price2 = $_POST['price2'];
+		system("python3 /var/www/html/final/python/test1.py {$price1} {$price2}"); 
+	?>
 	</form>
 </center>
 </body>
 </HTML>
-
-<!-- 
-select marketdate from stockprice order by marketdate desc limit 1;	//가장 최근 날짜 조회
-select * from stockprice where marketdate='변수(2017-01-10)'
-종목코드 날짜 doesprice(종가)
--->
--- web/graph.php
+++ web/graph.php
@@ -1,76 +1,142 @@
 <!--DB 연동-->
-<?
+<?/*
 	$db = mysqli_connect("pj9039.iptime.org:9039","root","rla123","stock");
 	if(mysqli_connect_errno($db)){
 		echo "연결실패 : ".mysqli_connect_error();
 	} else {
 		echo "성공";
-	}
-
+	}*/
 ?>
 <HTML>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <HEAD>
 	<title>개미추적자</title>
 
-	<style type="text/css">
-		table, td{
-			border:1px;
-		}
-		table{
-			width: 60%;
-			height: 100px;
-			margin: auto;
-			text-align: center;
-		}
-	</style>
-	
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="./js/bootstrap.min.js"></script>
+
+
+<style type="text/css">
+	th,td{padding: 15px;}
+	table{border-spacing: 5px;}
+	img:hover {
+   padding:1px;
+   border:1px solid #021a40;
+   background-color:#ff0;
+	}
+	.activeImg{
+		padding:1px;
+   border:1px solid #021a40;
+   background-color:#ff0;
+	}
+</style>
+<script>
+	function borderClick(bId){
+			var img = document.getElementById(bId);
+
+			for(var i = 1; i < 10; i++){
+				var imgTmp = document.getElementById("img"+i);
+				imgTmp.classList.remove("activeImg");
+			}
+
+			img.classList.add("activeImg");
+	}
+</script>
 </HEAD>
 <body>
 <center>
 	<BR>
 	<H1>그래프검색</H1>
 	<BR>
-	<!--그래프검색-->
-	<input type=button value=메인화면 onclick="location.href='main.php';"/><BR><BR>
 	<HR>
 	<form action="P_result.php" method="post">
-	<!--tr:한줄 th:글씨 진하게 td:글씨보통 colspan:가로줄을 합치는 개수 rowspan:세로줄을 합치는 개수-->
-	<table border=0 cellpadding=5 cellspacing=1>
+	<table style="width:1200">
 		<tr>
-			<th colspan="3" align="left"><font size="5"/>그래프</th>
-			<th colspan="3" align="left"><font size="5"/>추가조건</th>
+			<th align="center"><font size="5"/><span class="glyphicon glyphicon-check"></span> 그래프</th>
+			<th align="center"><font size="5"/><span class="glyphicon glyphicon-search"></span> 추가조건</th>
 		</tr>
-
-		<tr>
-			<td>그래프1</td>
-			<td>그래프2</td>
-			<td>그래프3</td>
-			<td> <input type=text name=price1/> </td>
-			<td> < 가격 < </td>
-			<td> <input type=text name=price2/> </td>
-		</tr>
-
-		<tr>
-			<td>그래프4</td>
-			<td>그래프5</td>
-			<td>그래프6</td>
-			<td> <input type=text name=roe1/> </td>
-			<td> < ROE < </td>
-			<td> <input type=text name=roe2/> </td>
-		</tr>
-
-		<tr>
-			<td>그래프7</td>
-			<td>그래프8</td>
-			<td>그래프9</td>
-			<td colspan="3" style="word-spacing: 15px"> <input type=submit value=검색> <input type=reset value=리셋> </td>
-		</tr>
-
-		
 	</table>
-
-
+	<!--tr:한줄 th:글씨 진하게 td:글씨보통 colspan:가로줄을 합치는 개수 rowspan:세로줄을 합치는 개수-->
+	<div style="display:table">
+	<div style="display:table-cell; text-align:center">
+	<div style="width: 600px; height: 500px; border: 1px; float: left;">
+	<table width="600" height="500" border="0" style="text-align: center;">
+	<tr>
+		<td>
+			<a href="javascript:borderClick('img1')"><img id="img1" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image1"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img2')"><img id="img2" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image2"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img3')"><img id="img3" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image3"/> -->
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<a href="javascript:borderClick('img4')"><img id="img4" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image4"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img5')"><img id="img5" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image5"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img6')"><img id="img6" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image6"/> -->
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<a href="javascript:borderClick('img7')"><img id="img7" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image7"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img8')"><img id="img8" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image8"/> -->
+		</td>
+		<td>
+			<a href="javascript:borderClick('img9')"><img id="img9" src="image/sample.png" width="150" height="100"></a>
+			<!-- <input type="checkbox" name="image9"/> -->
+		</td>
+	</tr>
+</table>
+	</div>
+	<div style="position: relative; width: 600px; height: 500px; border: 1px;  float: left; display:table;">
+	<div style="display:table-cell;vertical-align:middle;">
+	<table width="600" height="500" border="0" text-align: center;">
+	<tr style="vertical-align:bottom;">
+		<td colspan="3" style="text-align:left;"><font size="5"><span class="glyphicon glyphicon-expand"></span> 가격</font></td>
+	</tr>
+	<tr style="vertical-align:top;">
+		<td><input class="form-control" type=text name="price1" size="20" /></td>
+		<td><font size="5"/> ~ </td>
+		<td><input class="form-control" type=text name="price2" size="20"/></td>
+	</tr>
+	<tr style="vertical-align:bottom;">
+		<td colspan="3" style="text-align:left;"><font size="5"><span class="glyphicon glyphicon-expand"></span> ROE</font></td>
+	</tr>
+	<tr style="vertical-align:top;">
+			<td> <input class="form-control" type=text name="roe1" size="20"/> </td>
+			<td><font size="5"/> ~ </td>
+			<td> <input class="form-control" type=text name="roe2" size="20" /> </td>
+	</tr>
+</table>
+</div>
+</div>
+</div>
+</div>
+<table style="width:1200">
+	<tr>
+		<td  style="word-spacing: 15px" align="center"><input type="submit" class="btn btn-default" style="width: 300px; height:50px" value=검색> </td>	
+		<td  style="word-spacing: 15px" align="center"><input type="reset" class="btn btn-default" style="width: 300px; height: 50px" value=리셋> </td>
+	</tr>
+</table>
 	</form>
 </center>
 </body>
