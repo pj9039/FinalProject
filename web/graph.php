aaa7a935eaf18012075be0d94135c2d36a70f571
pj9039 <kms_14@naver.com>
2017-04-19 13:26:25 +0900

    Update graph.php

--- web/graph.php
+++ web/graph.php
@@ -1,76 +1,152 @@
-<!--DB 연동-->
-<?
-	$db = mysqli_connect("pj9039.iptime.org:9039","root","rla123","stock");
-	if(mysqli_connect_errno($db)){
-		echo "연결실패 : ".mysqli_connect_error();
-	} else {
-		echo "성공";
-	}
-
-?>
 <HTML>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <HEAD>
 	<title>개미추적자</title>
 
-	<style type="text/css">
-		table, td{
-			border:1px;
-		}
-		table{
-			width: 60%;
-			height: 100px;
-			margin: auto;
-			text-align: center;
-		}
-	</style>
-	
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="./js/bootstrap.min.js"></script>
+
+
+<style type="text/css">
+	th,td{padding: 15px;}
+	table{border-spacing: 5px;}
+	img:hover {
+   padding:1px;
+   border:1px solid #021a40;
+   background-color:#ff0;
+	}
+	.activeImg{
+		padding:1px;
+   border:1px solid #021a40;
+   background-color:#ff0;
+	}
+</style>
+<script>
+	function borderClick(bId){
+			var img = document.getElementById(bId);
+
+			for(var i = 1; i < 10; i++){
+				var imgTmp = document.getElementById("img"+i);
+				imgTmp.classList.remove("activeImg");
+			}
+
+			img.classList.add("activeImg");
+	}
+/*
+	$(document).ready(function(){
+		$("input:radio[name=radio").click(function(){
+			OnClick();
+		})
+	});
+	function OnClick(){
+
+	}*/
+</script>
 </HEAD>
 <body>
 <center>
 	<BR>
 	<H1>그래프검색</H1>
 	<BR>
-	<!--그래프검색-->
-	<input type=button value=메인화면 onclick="location.href='main.php';"/><BR><BR>
 	<HR>
 	<form action="P_result.php" method="post">
-	<!--tr:한줄 th:글씨 진하게 td:글씨보통 colspan:가로줄을 합치는 개수 rowspan:세로줄을 합치는 개수-->
-	<table border=0 cellpadding=5 cellspacing=1>
+	<table style="width:1200">
 		<tr>
-			<th colspan="3" align="left"><font size="5"/>그래프</th>
-			<th colspan="3" align="left"><font size="5"/>추가조건</th>
+			<th align="center"><font size="5"/><span class="glyphicon glyphicon-check"></span> 그래프</th>
+			<th align="center"><font size="5"/><span class="glyphicon glyphicon-search"></span> 추가조건</th>
 		</tr>
-
-		<tr>
-			<td>그래프1</td>
-			<td>그래프2</td>
-			<td>그래프3</td>
-			<td> <input type=text name=price1/> </td>
-			<td> < 가격 < </td>
-			<td> <input type=text name=price2/> </td>
-		</tr>
-
-		<tr>
-			<td>그래프4</td>
-			<td>그래프5</td>
-			<td>그래프6</td>
-			<td> <input type=text name=roe1/> </td>
-			<td> < ROE < </td>
-			<td> <input type=text name=roe2/> </td>
-		</tr>
-
-		<tr>
-			<td>그래프7</td>
-			<td>그래프8</td>
-			<td>그래프9</td>
-			<td colspan="3" style="word-spacing: 15px"> <input type=submit value=검색> <input type=reset value=리셋> </td>
-		</tr>
-
-		
 	</table>
-
-
+	<!--tr:한줄 th:글씨 진하게 td:글씨보통 colspan:가로줄을 합치는 개수 rowspan:세로줄을 합치는 개수-->
+	<div style="display:table">
+	<div style="display:table-cell; text-align:center">
+	<div style="width: 600px; height: 500px; border: 1px; float: left;">
+	<table width="600" height="500" border="0" style="text-align: center;">
+	<tr>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="1" />
+			<a href="javascript:borderClick('img1')"><img id="img1" src="image/pattern1.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="2" />
+			<a href="javascript:borderClick('img2')"><img id="img2" src="image/pattern2.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="3" />
+			<a href="javascript:borderClick('img3')"><img id="img3" src="image/pattern3.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="4" />
+			<a href="javascript:borderClick('img4')"><img id="img4" src="image/pattern4.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="5" />
+			<a href="javascript:borderClick('img5')"><img id="img5" src="image/pattern5.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="6" />
+			<a href="javascript:borderClick('img6')"><img id="img6" src="image/pattern6.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="8" />
+			<a href="javascript:borderClick('img7')"><img id="img7" src="image/pattern7.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="8" />
+			<a href="javascript:borderClick('img8')"><img id="img8" src="image/pattern8.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+		<td>
+			<label>
+  			<input type="radio" name="chkGraph" value="9" />
+			<a href="javascript:borderClick('img9')"><img id="img9" src="image/pattern9.PNG" width="150" height="100"></a>
+			</label>
+		</td>
+	</tr>
+</table>
+	</div>
+	<div style="position: relative; width: 600px; height: 500px; border: 1px;  float: left; display:table;">
+	<div style="display:table-cell;vertical-align:middle;">
+	<table width="600" height="500" border="0" text-align: center;">
+	<tr style="vertical-align:bottom;">
+		<td colspan="3" style="text-align:left;"><font size="5"><span class="glyphicon glyphicon-expand"></span> 가격</font></td>
+	</tr>
+	<tr style="vertical-align:top;">
+		<td><input class="form-control" type=text name="price1" size="20" /></td>
+		<td><font size="5"/> ~ </td>
+		<td><input class="form-control" type=text name="price2" size="20"/></td>
+	</tr>
+</table>
+</div>
+</div>
+</div>
+</div>
+<table style="width:1200">
+	<tr>
+		<td  style="word-spacing: 15px" align="center"><input type="submit" class="btn btn-default" style="width: 300px; height:50px" value=검색> </td>	
+		<td  style="word-spacing: 15px" align="center"><input type="reset" class="btn btn-default" style="width: 300px; height: 50px" value=리셋> </td>
+	</tr>
+</table>
 	</form>
 </center>
 </body>
