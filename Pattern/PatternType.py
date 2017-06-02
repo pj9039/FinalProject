1966923ba30bafead4740174091e5fdae06d9162
dbqhd345 <dbqhd345@naver.com>
2017-06-02 21:56:36 +0900

    Update PatternType.py
    그림 4,5,6 추가

--- Pattern/PatternType.py
+++ Pattern/PatternType.py
@@ -2,7 +2,9 @@ from fastpip import pip
 
 # pattern; sample array
 __PATTERN_DOUBLE_BOTTOM = [[0, 8000], [1, 4000], [2, 4000], [3, 8000], [4, 4000], [5, 4000], [6, 8000]]
-
+__TRIANGLES_DESCENDING = [[0, 8000], [1, 5500], [2, 6500], [3, 5500], [4, 6000], [5, 5000], [6, 4000]]
+__THREE_RIGING_VALLEYS = [[0, 6000], [1, 5500], [2, 6500], [3, 6000], [4, 7000], [5, 6500], [6, 8000]]
+__ROUNDING_BOTTOMS = [[0, 8000], [1, 6000], [2, 6500], [3, 5000], [4, 6500], [5, 6000], [6, 8000]]
 
 # Adapt pip algorithm & choose pattern by x value
 def patterntype(x):
@@ -13,11 +15,11 @@ def patterntype(x):
     if x == 3:      #
         return 3, 5
     if x == 4:      #
-        return 4, 7
+        return pip(__THREE_RIGING_VALLEYS, 7), 7
     if x == 5:      #
-        return 5, 7
+        return pip(__ROUNDING_BOTTOMS, 7), 7
     if x == 6:      #
-        return 6, 7
+        return pip(__TRIANGLES_DESCENDING, 7), 7
     if x == 7:      #
         return 7, 7
     if x == 8:      #
